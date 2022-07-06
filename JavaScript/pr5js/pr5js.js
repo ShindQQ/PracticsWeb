b873734635f70e6b5146b4ec709a88e0e39f0098
ShinQQ <denis110402@gmail.com>
2022-07-06 22:35:06 +0300

    Practic 5 js added

---
+++ JavaScript/pr5js/pr5js.js
@@ -0,0 +1,18 @@
+let operand1 = 10;
+let operand2 = 20;
+
+console.log(operand1 + operand2);
+console.log(operand1 - operand2);
+console.log(operand1 / operand2);
+console.log(operand1 * operand2);
+console.log(operand1 % operand2);
+console.log(operand1 ** operand2);
+console.log(++operand1);
+console.log(--operand2);
+console.log(operand1 < operand2);
+console.log(operand1 > operand2);
+console.log(operand1 == operand2);
+console.log(operand1 === operand2);
+console.log(operand1 != operand2);
+console.log(operand1 <= operand2);
+console.log(operand1 >= operand2);
